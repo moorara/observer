6f9085fdfe0d372e93fb3c3589f9d98258f903de
Milad Irannejad <milad@uber.com>
2020-04-23 01:41:20 -0400

    Bootstrap 🚀

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/moorara/observer
+
+go 1.14
---
+++ observer.go
@@ -0,0 +1,6 @@
+// Package observe can be used for implementing observability using OpenTelemetry API.
+// It aims to unify three pillars of observability in one single package that is easy-to-use and hard-to-misuse.
+//
+// An Observer encompasses a logger, a meter, and a tracer.
+// It offers a single unified developer experience for enabling observability.
+package observe
